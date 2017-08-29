a975e7037b0af790d7938ea938f53caeb8c7e069
chisod <stephen.j.odell@gmail.com>
2017-08-28 21:16:24 -0500

    ch7 work

---
+++ cpplearn/ch7/word_counter.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <map>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::string;
+
+int main()
+{
+	string s;
+	map<string, int> counters;
+
+	while (cin >> s)
+		++counters[s];
+
+	for (map<string, int>::const_iterator it = counters.begin();
+			it != counters.end(); ++it) {
+		cout << it->first << "\t" << it->second << endl;
+	}
+	return 0;
+}
